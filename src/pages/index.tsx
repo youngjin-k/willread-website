938bc470a98656219d55d7f3325222e89371bc17
youngjin-k <masichall@gmail.com>
2021-02-04 01:18:07 +0900

    feat: hero section 반응형 대응

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 import Layout from '../components/Layout';
+import containerStyle from '../styles/containerStyle';
+import media from '../styles/media';
 
 export default function Home() {
   return (
@@ -9,20 +11,23 @@ export default function Home() {
       }}
     >
       <HeroSection>
-        <ContentWrapper>
-          <HeroCopy>
-            글을 읽는
-            <br />
-            가벼운 습관,
-            <br />
-            윌리드
-          </HeroCopy>
-          <SubCopy>곧 만나요.</SubCopy>
-        </ContentWrapper>
-        <ImageWrapper>
-          <IphoneImage src="/assets/images/willread-home-iphone.png" />
-          <NotificationImage src="/assets/images/willread-notification-ios.png" />
-        </ImageWrapper>
+        <Conatiner>
+          <ContentWrapper>
+            <HeroCopy>
+              글을 읽는
+              <br />
+              가장 가벼운 습관,
+              <br />
+              윌리드
+            </HeroCopy>
+            <SubCopy>곧 만나요.</SubCopy>
+          </ContentWrapper>
+          <ImageWrapper>
+            <NotificationImage src="/assets/images/willread-notification-ios.png" />
+            <NotificationImageMobile src="/assets/images/willread-notification-ios-mobile.png" />
+            <IphoneImage src="/assets/images/willread-home-iphone.png" />
+          </ImageWrapper>
+        </Conatiner>
       </HeroSection>
     </Layout>
   );
@@ -35,10 +40,29 @@ const HeroSection = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
+
+  ${media.lessThan('large')} {
+    background: linear-gradient(166.88deg, #6e57ff 6.1%, #a498f1 99.33%);
+    height: auto;
+  }
+`;
+
+const Conatiner = styled.div`
+  ${containerStyle};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ${media.lessThan('large')} {
+    flex-direction: column;
+  }
 `;
 
 const ContentWrapper = styled.div`
-  margin: 0 120px 0 0;
+  ${media.lessThan('large')} {
+    flex-direction: column;
+  }
 `;
 
 const HeroCopy = styled.h1`
@@ -47,28 +71,64 @@ const HeroCopy = styled.h1`
   line-height: 70px;
   color: #ffffff;
   margin: 0;
+
+  ${media.lessThan('large')} {
+    font-size: 32px;
+    line-height: 1.4;
+    text-align: center;
+    margin: 112px 0 0 0;
+  }
 `;
 
 const SubCopy = styled.h3`
   font-size: 32px;
   color: rgb(221, 217, 243);
   margin: 32px 0 0 0;
+
+  ${media.lessThan('large')} {
+    display: none;
+  }
 `;
 
 const ImageWrapper = styled.div`
-  margin: 120px 0 0 120px;
+  margin: 48px 0 0 0;
   position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const NotificationImage = styled.img`
+  position: absolute;
+  top: 120px;
+  left: 157px;
+  width: 400px;
+  display: none;
+
+  ${media.greaterThan('large')} {
+    display: block;
+  }
+`;
+
+const NotificationImageMobile = styled.img`
+  position: relative;
+  top: auto;
+  left: auto;
+  width: 400px;
+  max-width: 100%;
+  display: block;
+
+  ${media.greaterThan('large')} {
+    display: none;
+  }
 `;
 
 const IphoneImage = styled.img`
   display: block;
   width: 344px;
-`;
+  max-width: 100%;
 
-const NotificationImage = styled.img`
-  position: absolute;
-  top: 87px;
-  left: 120px;
-  display: block;
-  width: 400px;
+  ${media.lessThan('large')} {
+    margin: 0 0 40px 0;
+  }
 `;
